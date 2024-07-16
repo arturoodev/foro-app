5fd2ca9a21ffe73f6118c1e229471cb3bce2af24
Arturo Pastrana <arturoo.dev@gmail.com>
2024-07-15 23:56:10 -0500

    Initial setup

---
+++ src/main/java/com/arturocode/appforo/AppforoApplication.java
@@ -0,0 +1,13 @@
+package com.arturocode.appforo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AppforoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AppforoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/arturocode/appforo/AppforoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.arturocode.appforo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AppforoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
